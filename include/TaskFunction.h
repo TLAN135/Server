e95374c70efe7957c756f92dac33ebf80d3ee1b9
Chen <yxkand135@163.com>
2024-05-15 16:53:14 +0800

    小修改

--- include/TaskFunction.h
+++ include/TaskFunction.h
@@ -19,10 +19,10 @@ class TaskFunction
     void (*const* vtable)() = nullptr;
 
     template<typename T>
-    friend void FunConstruct(Data& dst, T&& src)
+    static void FunConstruct(Data& dst, T&& src)
     {
         using U = typename std::decay<T>::type;
-        if (sizeof(U) > TaskFunction::max_size_)
+        if (sizeof(U) > max_size_)
             *(U**)&dst = new U(std::forward<T>(src));
 
         else {
@@ -30,82 +30,71 @@ class TaskFunction
         }
     }
     template<typename T>
-    friend inline void FunDestructor(Data& dst);
+    static void FunDestructor(TaskFunction::Data& dst)
+    {
+        using U = typename std::decay<T>::type;
+        if (sizeof(U) > TaskFunction::max_size_)
+            delete *(U**)&dst;
+        else
+            ((U*)&dst)->~U();
+    }
+
     template<typename T>
-    friend inline void FunMove(Data& dst, Data& src);
+    static void FunMove(Data& dst, Data& src)
+    {
+        using U = typename std::decay<T>::type;
+        if (sizeof(U) > max_size_) {
+            *(U**)&dst = *(U**)&src;
+            *(U**)&src = nullptr;
+        }
+        else
+            new ((U*)&dst) U(std::move(*(U*)&src));
+    }
+
     template<typename T>
-    friend inline void FunInvoke(const Data& dst);
+    static void FunInvoke(const Data& dst)
+    {
+        using U = typename std::decay<T>::type;
+        if (sizeof(U) > max_size_)
+            (**(U**)&dst)();
+        else
+            (*(U*)&dst)();
+    }
+
+    template<typename T>
+    static auto VtableInit() -> void (*const*)()
+    {
+        using FunType = void();
+        static void (*funs[3])() = {
+            (void (*)())FunDestructor<T>,
+            (void (*)())FunMove<T>,
+            (void (*)())FunInvoke<T>,
+        };
+        return funs;
+    }
 
 public:
     template<typename T>
-    TaskFunction(T&& obj);
+    TaskFunction(T&& obj)
+        : vtable(VtableInit<T>())
+    {
+        FunConstruct(data, std::forward<T>(obj));
+    }
+
+    TaskFunction(TaskFunction&& old)
+        : vtable(old.vtable)
+    {
+        old.vtable = nullptr;
+        ((void (*)(Data& otd, Data& src))vtable[1])(this->data, old.data);
+    }
+
     ~TaskFunction()
     {
         if (vtable) ((void (*)(Data&))vtable[0])(data);
     }
+
     void operator()()
     {
         ((void (*)(const Data&))vtable[2])(data);
     }
 };
-
-
-
-template<typename T>
-inline void FunDestructor(TaskFunction::Data& dst)
-{
-    using U = typename std::decay<T>::type;
-    if (sizeof(U) > TaskFunction::max_size_)
-        delete *(U**)&dst;
-    else
-        ((U*)&dst)->~U();
-}
-
-template<typename T>
-inline void FunMove(TaskFunction::Data& dst, TaskFunction::Data& src)
-{
-    using U = typename std::decay<T>::type;
-    if (sizeof(U) > TaskFunction::max_size_) {
-        *(U**)&dst = *(U**)&src;
-        *(U**)&src = nullptr;
-    }
-    else
-        new ((U*)&dst) U(std::move(*(U*)&src));
-}
-
-template<typename T>
-inline void FunInvoke(const TaskFunction::Data& dst)
-{
-    using U = typename std::decay<T>::type;
-    if (sizeof(U) > TaskFunction::max_size_)
-        (**(U**)&dst)();
-    else
-        (*(U*)&dst)();
-}
-
-template<typename T>
-inline auto VtableInit() -> void (*const*)()
-{
-    using FunType = void();
-    static void (*funs[3])() = {
-        (void (*)())FunDestructor<T>,
-        (void (*)())FunMove<T>,
-        (void (*)())FunInvoke<T>,
-    };
-    return funs;
-}
-
-template<typename T>
-inline TaskFunction::TaskFunction(T&& obj)
-    : vtable(VtableInit<T>())
-{
-    FunConstruct(data, std::forward<T>(obj));
-}
-
-template<>
-inline TaskFunction::TaskFunction(TaskFunction&& old)
-    : vtable(old.vtable)
-{
-    old.vtable = nullptr;
-    ((void (*)(Data& otd, Data& src))vtable[1])(this->data, old.data);
-}
